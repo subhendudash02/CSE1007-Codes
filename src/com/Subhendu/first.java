8841de5aef26ac40d6394dd2c6220d50b6d5cbd3
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-01-05 00:43:58 +0530

    Removing out and fixning the dir

---
+++ src/com/Subhendu/first.java
@@ -0,0 +1,17 @@
+package com.Subhendu;
+
+public class first {
+    public static void main(String[] args) {
+        // lol first program
+        System.out.println("First Program");
+        int arr[] = {1, 2, 3, 4, 5};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        int a = 5;
+        double b = 6.0;
+        System.out.println(a + b);
+    }
+}
