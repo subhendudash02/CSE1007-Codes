b6dd095c6bde571d12a8bf7cbb829151fddb2b60
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-02-08 22:38:48 +0530

    Lab-1 Work

--- src/Lab_1/question_1.java
+++ src/Lab_1/question_1.java
@@ -1,2 +1,30 @@
-package Lab_1;public class question_1 {
+package Lab_1;
+
+import java.util.Scanner;
+
+public class question_1 {
+    // fibonacci series
+    static int fibonacci(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        else if (n == 2) {
+            return 1;
+        }
+        else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int terms = obj.nextInt();
+
+        System.out.print("\n");
+
+        for (int i = 1; i <= terms + 1; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
 }
